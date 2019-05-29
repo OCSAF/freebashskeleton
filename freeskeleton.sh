d1d8a465e54a12d6e1b56b76f62753c65f0aecfa
MG() <freecybersecurity@protonmail.com>
2019-05-29 19:39:09 +0200

    freebashskeleton 0.3

--- freeskeleton.sh
+++ freeskeleton.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #################################################################################
-################### FREE - Skeleton Version 0.2 - under GPLv3 ###################
+################### FREE - Skeleton Version 0.3 - under GPLv3 ###################
 ################### by Mathias Gut, Netchange Informatik GmbH ###################
 ################### From the freecybersecurity.org Project    ###################
 ################### Thanks to the community for the ideas     ###################
@@ -29,23 +29,36 @@ program=(python3 vi emacs)
 for i in "${program[@]}"; do
 	if [ -z $(command -v ${i}) ]; then
 		echo "${i} is not installed."
+		count=1
+	fi
+	
+	if [[ $count -eq 1 ]]; then
 		exit
 	fi
 done
+unset program
+unset count
 
 #Check if a program is installed via an input file.
 while read line
 do
-	unset program
-
 	program=$(echo "$line" | awk -F ';;' '{print $1}')
+	proginst=$(echo "$line" | awk -F ';;' '{print $2}')
 
 	if [ -z $(command -v ${program}) ]; then
-		echo "${program} is not installed."
-		exit
+		echo "${program} is not installed. Installation: ${proginst}"
+		count=1
 	fi
 
+	unset program
+	unset proginst
+
 done <./commandcheck.txt
+
+if [[ $count -eq 1 ]]; then
+	unset count
+	exit
+fi
 
 #Read current date and time in hours and minutes into variable.
 time=$(date +%d.%m.%Y-%H:%M)
@@ -69,7 +82,7 @@ fi
 
 usage() {
 	echo "From the Free OCSAF project"
-	echo "Free OCSAF SKELETON 0.1 - GPLv3 (https://freecybersecurity.org)"
+	echo "Free OCSAF SKELETON 0.3 - GPLv3 (https://freecybersecurity.org)"
 	echo "Use only with legal authorization and at your own risk!"
        	echo "ANY LIABILITY WILL BE REJECTED!"
        	echo ""
@@ -108,7 +121,7 @@ while getopts "d:l:hc" opt; do
   	done
 	shift $(( OPTIND - 1 ))
 
-#Check if opt_arg1 or opt_arg2 is set
+#Check if opt_arg1 or/and opt_arg2 is set
 if [ "$opt_arg1" == "" ] && [ "$opt_arg2" == "" ]; then
 	echo "**No argument set**"
 	echo ""
@@ -121,10 +134,27 @@ fi
 ### COLORS  ###
 ###############
 
+#Colors directly in the script.
+if [[ $colors -eq 1 ]]; then
+	cOFF=''
+	rON=''
+	gON=''
+	yON=''
+else
+	cOFF='\e[39m'	  #color OFF / Default color
+	rON='\e[31m'	  #red color ON
+	gON='\e[32m'	  #green color ON
+	yON='\e[33m'	  #yellow color ON
+fi
+
+
+#As color library.
 . colors.sh
 
 
-################### your cool functions ####################
+############################
+#### your cool functions ###
+############################
 
 # My function for ...
 # Naming convention for functions funcFunctionname() - z.B. funcMycheck()
@@ -138,12 +168,15 @@ funcMyfuction() {
 }
 
 
-####### MAIN PROGRAM #######
+############
+### MAIN ###
+############
 
 echo ""
-echo "#########################"
-echo "####  FREE SKELETON  ####"
-echo "#########################"
+echo "##########################################"
+echo "####  FREE OCSAF BASH SKELETON GPLv3  ####"
+echo "####  https://freecybersecurity.org   ####"
+echo "##########################################"
 echo ""
 
 if [ "$opt_arg1" == "1" ]; then        #For one argument
